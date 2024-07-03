461578263a084a0e63bc1982f75b8395c130380e
joshua-m-vaughan <50953792+joshua-m-vaughan@users.noreply.github.com>
2024-07-03 16:21:40 -0700

    Instantiate game rules class

--- ExtendedFormGame/template.py
+++ ExtendedFormGame/template.py
@@ -22,4 +22,45 @@ class GameState():
 class Action():
     pass
 
+class GameRule:
+    def __init__(self, num_agents = 2):
+        self.current_agent_index = 0
+        self.num_agents = num_agents
+        self.current_game_state = self.initial_game_state()
+        self.action_counter = 0
+
+    def initial_game_state(self):
+        utils.raiseNotDefined()
+        return 0
+
+    def generate_successor(self, game_state, action, agent_id):
+        utils.raiseNotDefined()
+        return 0
+
+    def get_next_agent_index(self):
+        return (self.current_agent_index + 1) % self.num_of_agent
+
+    def get_legal_actions(self, game_state, agent_id):
+        utils.raiseNotDefined()
+        return []
+
+    def calculate_score(self, game_state,agent_id):
+        utils.raiseNotDefined()
+        return 0
+
+    def game_ends(self):
+        utils.raiseNotDefined()
+        return False
+
+    def update(self, action):
+        temp_state = self.current_game_state
+        self.current_game_state = self.generate_successor(temp_state,
+                                                          action,
+                                                          self.current_agent_index)
+        self.current_agent_index = self.get_next_agent_index()
+        self.action_counter += 1
+
+    def getCurrentAgentIndex(self):
+        return self.current_agent_index
+
 # END ---------------------------------------------------------------- #
