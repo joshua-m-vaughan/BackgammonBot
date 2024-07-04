5b78df908e1b7f350d44e51aa45ff1bbfeb93b2f
joshua-m-vaughan <50953792+joshua-m-vaughan@users.noreply.github.com>
2024-07-03 17:03:06 -0700

    Implement structure of game that models Backgammon

---
+++ Backgammon/backgammon_model.py
@@ -0,0 +1,28 @@
+# INFORMATION -------------------------------------------------------- #
+
+# Author:  Josh Vaughan, leveraging the extended form game framework
+#          from COMP90054 Assignment 3.
+# Date:    03/07/2024
+# Purpose: Implements Backgammon for the purposes of developing an AI
+#          agent for ME344: Introduction to building High Performance
+#          Computing clusters, at Stanford Summer Session 2024.
+
+# IMPORTS ------------------------------------------------------------ #
+
+from ExtendedFormGame.template import GameState, GameRules, Action
+
+# CONSTANTS ---------------------------------------------------------- #
+
+# CLASS DEF ---------------------------------------------------------- #
+
+class BackgammonState(GameState):
+    pass
+
+class BackgammonRules(GameRules):
+    pass
+
+class BackgammonAction(Action):
+    pass
+
+
+# END ---------------------------------------------------------------- #
