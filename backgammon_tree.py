9614baeddd52178fc267f50417c6e77297614686
joshua-m-vaughan <50953792+joshua-m-vaughan@users.noreply.github.com>
2024-07-05 22:13:58 -0700

    Implement getters for PlayNode class

--- backgammon_tree.py
+++ backgammon_tree.py
@@ -19,7 +19,7 @@ class PlayNode():
     next_node_id = 0
 
     def __init__(self, parent, state:BackgammonState,
-                 action:tuple = None) -> None:
+                 move:tuple = None) -> None:
         
         # Assign node an ID.
         self.id = PlayNode.next_node_id
@@ -29,9 +29,44 @@ class PlayNode():
         self._parent = parent
         self._children = None # TODO: Confirm if this is correct.
         self._state = state
-        self._action = action
+        self._move = move
 
         return None
     
+    def get_parent(self):
+        """get_parent
+        Returns the parent attribute of the PlayNode instance.
 
+        Returns:
+            PlayNode: Parent node of current node.
+        """
+        return self._parent
+    
+    def get_children(self):
+        """get_children
+        Returns the children attribute of the PlayNode instance.
+
+        Returns:
+            list[PlayNode]: List of children nodes of current node.
+        """
+        return self._children
+    
+    def get_state(self):
+        """get_state
+        Returns the state attribute of the PlayNode instance.
+
+        Returns:
+            BackgammonState: Backgammon state.
+        """
+        return self._state
+    
+    def get_move(self):
+        """get_action
+        Returns the move attribute of the PlayNode instance.
+
+        Returns:
+            tuple: Two tuple detailing fromPoint and toPoint of a move.
+        """
+        return self._move
+    
 # END ---------------------------------------------------------------- #
