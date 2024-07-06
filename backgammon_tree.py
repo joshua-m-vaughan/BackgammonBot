15a8cd576cf608969a6721d6e680be61a856bff6
joshua-m-vaughan <50953792+joshua-m-vaughan@users.noreply.github.com>
2024-07-06 13:39:30 -0700

    Remove bloat of getters/setters that is not required in Python

--- backgammon_tree.py
+++ backgammon_tree.py
@@ -26,48 +26,9 @@ class PlayNode():
         PlayNode.next_node_id += 1
 
         # Assign attribute values.
-        self._parent = parent
-        self._children = None # TODO: Confirm if this is correct.
-        self._state = state
-        self._move = move
-
-        return None
-    
-    def get_parent(self):
-        """get_parent
-        Returns the parent attribute of the PlayNode instance.
-
-        Returns:
-            PlayNode: Parent node of current node.
-        """
-        return self._parent
-    
-    def get_children(self):
-        """get_children
-        Returns the children attribute of the PlayNode instance.
-
-        Returns:
-            list[PlayNode]: List of children nodes of current node.
-        """
-        return self._children
-    
-    def get_state(self):
-        """get_state
-        Returns the state attribute of the PlayNode instance.
-
-        Returns:
-            BackgammonState: Backgammon state.
-        """
-        return self._state
-    
-    def get_move(self):
-        """get_action
-        Returns the move attribute of the PlayNode instance.
-
-        Returns:
-            tuple: Three tuple detailing fromPoint, toPoint, and face
-            value of a move.
-        """
-        return self._move
+        self.parent = parent
+        self.children = None # TODO: Confirm if this is correct.
+        self.state = state
+        self.move = move
     
 # END ---------------------------------------------------------------- #
