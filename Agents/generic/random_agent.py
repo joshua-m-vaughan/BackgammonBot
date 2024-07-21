7b00cbf3757724764ce406b2a5c4bae5dce7261d
joshua-m-vaughan <50953792+joshua-m-vaughan@users.noreply.github.com>
2024-07-21 14:02:55 -0700

    Random agent implementation for a generic game

---
+++ Agents/generic/random_agent.py
@@ -0,0 +1,25 @@
+# INFORMATION ------------------------------------------------------------------------------------------------------- #
+
+# Author:  Josh Vaughan, leveraging the extended form game framework
+#          from COMP90054 Assignment 3.
+# Date:    21/07/2024
+# Purpose: Implements a Random agent for a generic board game.
+
+# IMPORTS ------------------------------------------------------------------------------------------------------------#
+
+from ExtendedFormGame.template import Agent
+import random
+
+# CONSTANTS ----------------------------------------------------------------------------------------------------------#
+
+
+# CLASS DEF ----------------------------------------------------------------------------------------------------------#  
+
+class myAgent(Agent):
+    def __init__(self,_id: int) -> None:
+        super().__init__(_id)
+    
+    def SelectAction(self,actions,game_state):
+        return random.choice(actions)
+    
+# END ---------------------------------------------------------------- #
