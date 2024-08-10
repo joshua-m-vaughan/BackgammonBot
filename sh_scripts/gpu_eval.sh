e4da424b011f1e0405a4ffa73b88eacc326d6e3c
joshua-m-vaughan <50953792+joshua-m-vaughan@users.noreply.github.com>
2024-08-10 14:53:27 -0700

    Update bulk evaluation request script

--- sh_scripts/gpu_eval.sh
+++ sh_scripts/gpu_eval.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-numGames=5000
+numGames=500
 seed=344
-agent1000=Agents/rl/tdgammon/trained_models/20240801-0052_tdgammon0_0_selfplay_cpu1000.pt
-agent5000=Agents/rl/tdgammon/trained_models/20240801-1949_tdgammon0_0_selfplay_gpu5000.pt
-agent15000=Agents/rl/tdgammon/trained_models/20240801-0750_tdgammon0_0_selfplay_cpu15000.pt
-evalPath=results/eval
+agent500=Agents/rl/tdgammon/trained_models/20240810-1153_tdg00_sp_cpu500.pt
+agent1000=Agents/rl/tdgammon/trained_models/20240810-1113_tdg00_sp_cpu1000.pt
+agent2000=Agents/rl/tdgammon/trained_models/20240810-1005_tdg00_sp_cpu2000.pt
+evalPath=results/eval/presentation_results
 
-sbatch sh_scripts/bg_bot_evalgpu_submit.slurm $numGames $seed $agent1000 $agent1000 $evalPath/20240802_1000v1000
-sbatch sh_scripts/bg_bot_evalgpu_submit.slurm $numGames $seed $agent1000 $agent5000 $evalPath/20240802_1000v5000
-sbatch sh_scripts/bg_bot_evalgpu_submit.slurm $numGames $seed $agent1000 $agent15000 $evalPath/20240802_1000v15000
+sbatch sh_scripts/bg_bot_evalgpu_submit.slurm $numGames $seed $agent500 $agent500 $evalPath
+sbatch sh_scripts/bg_bot_evalgpu_submit.slurm $numGames $seed $agent500 $agent1000 $evalPath
+sbatch sh_scripts/bg_bot_evalgpu_submit.slurm $numGames $seed $agent500 $agent2000 $evalPath
 
-sbatch sh_scripts/bg_bot_evalgpu_submit.slurm $numGames $seed $agent5000 $agent1000 $evalPath/20240802_5000v1000
-sbatch sh_scripts/bg_bot_evalgpu_submit.slurm $numGames $seed $agent5000 $agent5000 $evalPath/20240802_5000v5000
-sbatch sh_scripts/bg_bot_evalgpu_submit.slurm $numGames $seed $agent5000 $agent15000 $evalPath/20240802_5000v15000
+sbatch sh_scripts/bg_bot_evalgpu_submit.slurm $numGames $seed $agent1000 $agent500 $evalPath
+sbatch sh_scripts/bg_bot_evalgpu_submit.slurm $numGames $seed $agent1000 $agent1000 $evalPath
+sbatch sh_scripts/bg_bot_evalgpu_submit.slurm $numGames $seed $agent1000 $agent2000 $evalPath
 
-sbatch sh_scripts/bg_bot_evalgpu_submit.slurm $numGames $seed $agent15000 $agent1000 $evalPath/20240802_15000v1000
-sbatch sh_scripts/bg_bot_evalgpu_submit.slurm $numGames $seed $agent15000 $agent5000 $evalPath/20240802_15000v5000
-sbatch sh_scripts/bg_bot_evalgpu_submit.slurm $numGames $seed $agent15000 $agent15000 $evalPath/20240802_15000v15000
+sbatch sh_scripts/bg_bot_evalgpu_submit.slurm $numGames $seed $agent2000 $agent500 $evalPath
+sbatch sh_scripts/bg_bot_evalgpu_submit.slurm $numGames $seed $agent2000 $agent1000 $evalPath
+sbatch sh_scripts/bg_bot_evalgpu_submit.slurm $numGames $seed $agent2000 $agent2000 $evalPath
